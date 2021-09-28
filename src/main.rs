917e71fd810acb561e2066ab230972e66c42a822
Philipp Joram <mail@phijor.me>
2021-09-28 22:38:28 +0300

    feat: Support fetching cats and birds, too
    Just type "5 cats", "bird" or "1 bird" to get a list of random animals
    that are not Shiba Inus.

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use tokio_stream::wrappers::UnboundedReceiverStream;
 use tracing::{debug, error, info, warn};
 use tracing_subscriber::FmtSubscriber;
 
-use std::{borrow::Cow, time::Duration};
+use std::{borrow::Cow, str::FromStr, time::Duration};
 
 static APP_USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"),);
 
@@ -65,10 +65,11 @@ impl ShibeInlineQueryHandler {
             "Inline query received from @{}",
             Self::extract_username(update)
         );
-        let count: usize = update.query.parse().unwrap_or(5);
+
+        let query: ShibeQuery = update.query.parse().unwrap_or_default();
 
         let shibes = self
-            .fetch_shibes(count)
+            .fetch(query.ep, query.count)
             .await
             .context("Failed to fetch shibes")?;
 
@@ -81,15 +82,18 @@ impl ShibeInlineQueryHandler {
         Ok(())
     }
 
-    const API_URL: &'static str = "https://shibe.online/api/shibes";
+    const API_URL: &'static str = "https://shibe.online/api";
 
     #[tracing::instrument(level = "debug", skip(self))]
-    async fn fetch_shibes(self, count: usize) -> Result<Vec<InlineQueryResult>> {
-        debug!("Fetching shibes");
+    async fn fetch(self, ep: ApiEndpoint, count: Option<usize>) -> Result<Vec<InlineQueryResult>> {
+        let ep = ep.as_str();
+        debug!("Fetching {}", ep);
+
+        let count = count.unwrap_or(5).min(25);
 
         let response = self
             .client
-            .get(Self::API_URL)
+            .get(format!("{}/{}", Self::API_URL, ep))
             .query(&[("count", count)])
             .send()
             .await
@@ -97,10 +101,7 @@ impl ShibeInlineQueryHandler {
 
         debug!("Received API response...");
 
-        let urls: Vec<String> = response
-            .json()
-            .await
-            .context("Failed to parse shibe result")?;
+        let urls: Vec<String> = response.json().await.context("Failed to parse response")?;
 
         debug!("Parsed result JSON...");
 
@@ -120,7 +121,7 @@ impl ShibeInlineQueryHandler {
             })
             .collect();
 
-        debug!(total = result.len(), "Finished fetching shibes.");
+        debug!(total = result.len(), "Finished fetching {}.", ep);
 
         Ok(result)
     }
@@ -160,5 +161,118 @@ impl DispatcherHandler<AutoSend<Bot>, InlineQuery> for ShibeInlineQueryHandler {
                 }
             })
             .boxed()
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum ApiEndpoint {
+    Shibes,
+    Cats,
+    Birds,
+}
+
+impl Default for ApiEndpoint {
+    fn default() -> Self {
+        Self::Shibes
+    }
+}
+
+impl ApiEndpoint {
+    fn as_str(&self) -> &'static str {
+        match self {
+            Self::Shibes => "shibes",
+            Self::Cats => "cats",
+            Self::Birds => "birds",
+        }
+    }
+}
+
+impl FromStr for ApiEndpoint {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let ep = match s {
+            "shibe" | "shibes" | "shiba" | "shibas" => ApiEndpoint::Shibes,
+            "cat" | "cats" => ApiEndpoint::Cats,
+            "bird" | "birds" => ApiEndpoint::Birds,
+            _ => return Err(()),
+        };
+
+        Ok(ep)
+    }
+}
+
+#[derive(Debug, PartialEq)]
+struct ShibeQuery {
+    count: Option<usize>,
+    ep: ApiEndpoint,
+}
+
+impl Default for ShibeQuery {
+    fn default() -> Self {
+        Self {
+            count: None,
+            ep: ApiEndpoint::Shibes,
+        }
+    }
+}
+
+impl FromStr for ShibeQuery {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut parts = s.split_whitespace();
+
+        let query = match (parts.next(), parts.next()) {
+            (Some(count), Some(ep)) => {
+                let count = count.parse().ok();
+                let ep = ApiEndpoint::from_str(ep).map_err(drop)?;
+
+                Self { count, ep }
+            }
+            (Some(ep), None) => Self {
+                count: None,
+                ep: ApiEndpoint::from_str(ep).unwrap_or_default(),
+            },
+            _ => return Err(()),
+        };
+
+        Ok(query)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod shibe_query {
+        use super::*;
+
+        #[test]
+        fn parse_full() {
+            assert_eq!(
+                "5 cats".parse(),
+                Ok(ShibeQuery {
+                    count: Some(5),
+                    ep: ApiEndpoint::Cats
+                })
+            )
+        }
+
+        #[test]
+        fn parse_omit_count() {
+            assert_eq!(
+                "cats".parse(),
+                Ok(ShibeQuery {
+                    count: None,
+                    ep: ApiEndpoint::Cats
+                })
+            )
+        }
+
+        #[test]
+        fn parse_fail_empty() {
+            assert_eq!("".parse::<ShibeQuery>(), Err(()));
+        }
     }
 }
